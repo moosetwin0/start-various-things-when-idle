6ad259db7171eed8240fe69ef12a371dbc524fd0
moosetwin0 <82544732+moosetwin0@users.noreply.github.com>
2024-05-06 12:51:07 -0400

    made code less "my pc only"
    I suck at titles

--- on idle start archiveteam and torrent client.py
+++ on idle start archiveteam and torrent client.py
@@ -2,17 +2,25 @@ import os
 import win32api
 from time import sleep
 
-# runs programs, assumes that you are actually idle
-os.popen('"C:\\Program Files\\WireGuard\\wireguard.exe" /installtunnelservice "C:\\Program Files\\WireGuard\\Data\\Configurations\\wgcf-profile.conf.dpapi"')
+# important! 
+# replace text where shown with the actual names/paths for the files 
+# replace \ with \\ or python will get confused
+# the replaced text must be in quotes "like this"
+wgcf = "REPLACE THIS TEXT WITH WIREGUARD PATH" # example: "C:\\Program Files\\WireGuard\\Data\\Configurations\\wgcf-profile.conf.dpapi"
+atvm = "REPLACE THIS TEXT WITH THE NAME OF THE ARCHIVETEAM VM" # example: "archiveteam-warrior-3.2"
+
+# runs the programs, assumes that you are actually idle
+os.popen(f'"C:\\Program Files\\WireGuard\\wireguard.exe" /installtunnelservice "{wgp}"')
 os.popen('"C:\\Program Files\\qBittorrent\\qbittorrent.exe"')
-os.popen('"C:\\Program Files\\Oracle\\VirtualBox\\VBoxManage.exe" startvm "archiveteam-warrior-3.2"')
+os.popen(f'"C:\\Program Files\\Oracle\\VirtualBox\\VBoxManage.exe" startvm "{atvm}"')
 
 # for closing programs when coming out of idle, shamefully stolen from stackoverflow
 fixedposition = win32api.GetCursorPos()
 while(True):
     position = win32api.GetCursorPos()
     if fixedposition != position:
-        os.popen('"C:\\Program Files\\Oracle\\VirtualBox\\VBoxManage.exe" controlvm "archiveteam-warrior-3.2" acpipowerbutton')
+        os.popen(f'"C:\\Program Files\\Oracle\\VirtualBox\\VBoxManage.exe" controlvm "{atvm}" acpipowerbutton')
         os.popen('taskkill /im "qbittorrent.exe"')
+        os.popen(f'"C:\\Program Files\\WireGuard\\wireguard.exe" /uninstalltunnelservice "{wgp}"')
         exit()
     sleep(0.05)
