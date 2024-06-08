bfbec14f1f97912a93e3d16bb0e2913afff8a7c5
moosetwin0 <82544732+moosetwin0@users.noreply.github.com>
2024-06-08 06:28:44 -0400

    Update on idle start archiveteam and torrent client.py

--- on idle start archiveteam and torrent client.py
+++ on idle start archiveteam and torrent client.py
@@ -1,22 +1,34 @@
 import os
 import win32api
+import win32gui
 import configparser
+from ctypes import windll
 from time import sleep
-import subprocess
+
+# check if window is fullscreen, shamefully stolen from stackoverflow
+def Fullscreen():
+    user32 = windll.user32
+    user32.SetProcessDPIAware()
+    screensize = (0, 0, user32.GetSystemMetrics(0), user32.GetSystemMetrics(1))
+    windowsize = win32gui.GetWindowRect(user32.GetForegroundWindow())
+    if screensize == windowsize:
+        return True
+    else:
+        return False
 
 config = configparser.ConfigParser(allow_no_value=True)
 
 # checks if config file exists, if it doesn't, create new one with booleanss, if it does, read it
 if not os.path.isfile('cfg.ini'):
     # defauits
-    config['booleans'] = {'# change to 1 for on, 0 for off':None, 'wireguard':'1', 'wireguard turns off after idle ends':'1', 'qbittorrent':'1', 'qbittorrent turns off after idle ends':'1', 'archiveteam VM':'1', 'archiveteam VM turns off after idle ends':'1', 'ytsync':'1'}
+    config['booleans'] = {'# change to True for on, False for off':None, 'do not detect idle while fullscreen':True,'wireguard on/off':'True', 'wireguard turns off after idle ends':'True', 'qbittorrent on/off':True, 'qbittorrent turns off after idle ends':True, 'archiveteam VM on/off':True, 'archiveteam VM turns off after idle ends':True, 'ytsync on/off':True}
     config['paths'] = {'# you do not need to fill in ones that are turned off in the boolean section!':None, 'wireguard executable path':r'C:\Program Files\WireGuard\wireguard.exe', 'wireguard config path':r'C:\Program Files\WireGuard\Data\Configurations\wgcf-profile.conf.dpapi', 'qbittorrent executable path':r'C:\Program Files\qBittorrent\qbittorrent.exe', 'virtualbox executable path':r'C:\Program Files\Oracle\VirtualBox\VBoxManage.exe', 'ytsync path':r'G:\music\playlist sync.lnk'}
     config['misc'] = {'archiveteam warrior vm name':'archiveteam-warrior-3.2', '# the duration should be in seconds':None, 'time before idle':'1200'}
     # creating file requires admin, reading might too
     config.write(open('cfg.ini', 'w'))
 else: config.read('cfg.ini')
 
-while(True): # this is so that the idle checking still continues if the computer becomes temporarily active
+while(True): # this is so that the idle checking still continues if the computer becomes (temporarily) active
     fixedposition = win32api.GetCursorPos() # a lot of this type of code could probably be put into a dedicated function but I can't be bothered
     mousetimer = 0
     # increases mousetimer until it reaches a set value, mouse movement resets it to 0
@@ -25,39 +37,35 @@ while(True): # this is so that the idle checking still continues if the computer
         if fixedposition != position:
             mousetimer = 0
             fixedposition = win32api.GetCursorPos()
+        if config['booleans']['do not detect idle while fullscreen'] == True and Fullscreen == True:
+            mousetimer = 0
+            # mousetimer will constantly be at 0.05 if fullscreen because of above and below but it does not matter
         mousetimer = round((mousetimer + 0.05), 2) # rounding is to remove floating point errors, not very important but it looks nice
         sleep(0.05)
 
     # runs the programs, REQUIRES ADMIN
     # these should be done without the repeated if statements but I (mistakenly) thought it wouldn't matter
-    # subprocess doesn't like working in the background so it is commented out
-    if int(config['booleans']['wireguard']) == 1:
+    # subprocess doesn't like working in the background so popen is used instead
+    if config['booleans']['wireguard on/off'] == True:
         os.popen(fr'"{config['paths']['wireguard executable path']}" /installtunnelservice "{config['paths']['wireguard config path']}"')
-        # subprocess.Popen(fr'"{config['paths']['wireguard executable path']}" /installtunnelservice "{config['paths']['wireguard config path']}"',shell=False, stdin=None, stdout=None, stderr=None)
-    if int(config['booleans']['qbittorrent']) == 1:
+    if config['booleans']['qbittorrent on/off'] == True:
         os.popen(fr'"{config['paths']['qbittorrent executable path']}"')
-        # subprocess.Popen(fr'"{config['paths']['qbittorrent executable path']}"',shell=False, stdin=None, stdout=None, stderr=None)
-    if int(config['booleans']['archiveteam VM']) == 1:
+    if config['booleans']['archiveteam VM on/off'] == True:
         os.popen(fr'"{config['paths']['virtualbox executable path']}" startvm "{config['misc']['archiveteam warrior vm name']}"')
-        # subprocess.Popen(fr'"{config['paths']['virtualbox executable path']}" startvm "{config['misc']['archiveteam warrior vm name']}"',shell=False, stdin=None, stdout=None, stderr=None)
-    if int(config['booleans']['ytsync']) == 1:
+    if config['booleans']['ytsync on/off'] == True:
         os.popen(fr'"{config['paths']['ytsync path']}"')
-        # subprocess.Popen(fr'"{config['paths']['ytsync path']}"',shell=True, stdin=None, stdout=None, stderr=None)
 
-    # for closing programs when coming out of idle, shamefully stolen from stackoverflow
+    # for closing programs when coming out of idle, adapted from stackoverflow
     # also could be done without the repeated if statements
     fixedposition = win32api.GetCursorPos()
     while(True):
         position = win32api.GetCursorPos()
         if fixedposition != position: # this code also requires admin unfortunately
-            if int(config['booleans']['archiveteam VM turns off after idle ends']) == 1:
+            if config['booleans']['archiveteam VM turns off after idle ends'] == True:
                 os.popen(fr'"{config['paths']['virtualbox executable path']}" controlvm "{config['misc']['archiveteam warrior vm name']}" acpipowerbutton')
-                # subprocess.Popen(fr'"{config['paths']['virtualbox executable path']}" controlvm "{config['misc']['archiveteam warrior vm name']}" acpipowerbutton',shell=False, stdin=None, stdout = None, stderr=None)
-            if int(config['booleans']['qbittorrent turns off after idle ends']) == 1:
+            if config['booleans']['qbittorrent turns off after idle ends'] == True:
                 os.popen('taskkill /im "qbittorrent.exe"')
-                # subprocess.Popen('taskkill /im "qbittorrent.exe"',shell=False, stdin=None, stdout=None, stderr=None)
-            if int(config['booleans']['wireguard turns off after idle ends']) == 1:
+            if config['booleans']['wireguard turns off after idle ends'] == True:
                 os.popen(fr'"{config['paths']['wireguard executable path']}" /uninstalltunnelservice "{config['paths']['wireguard config path']}"')
-                # subprocess.Popen(fr'"{config['paths']['wireguard executable path']}" /uninstalltunnelservice "{config['paths']['wireguard config path']}"',shell=False, stdin=None, stdout=None, stderr=None)
             break
         sleep(0.05)
