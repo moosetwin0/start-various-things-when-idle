78f06489098c0a4e1af0aff186c38a1858dcb351
moosetwin0 <82544732+moosetwin0@users.noreply.github.com>
2024-07-14 01:23:55 -0400

    added feature for it optionally to not run if a program is running
    I have a headache

--- on idle start archiveteam and torrent client.py
+++ on idle start archiveteam and torrent client.py
@@ -1,16 +1,31 @@
+# there are a lot of dependancies that could be merged here, TODO
 import os
 import win32api
 import win32gui
 import configparser
-from ctypes import windll
 from time import sleep
+# TODO find out a way to make savepagenow and psutil optional depenencies(?)
 import savepagenow
+import psutil
+# it makes me a bit uncomfortable having a dependancy that no regular user will use, it should come pre-packaged with python but I still feel uncomfortable, TODO 
+#from timeit import timeit # silly
 
-# check if running in vscode and if so set working directory so I can debug in vscode without random errors
-# also stolen from stackoverflow
+# I spent 3 hours trying to figure this out, I ended up copying code from the internet but if I touch it the code stops working and I can't figure out why
+# oh yeah I copied it from https://thispointer.com/python-check-if-a-process-is-running-by-name-and-find-its-process-id-pid/
+def checkIfProcessRunning(processName):
+    for proc in psutil.process_iter():
+        try:
+            if processName.lower() in proc.name().lower() and processName != '':
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return False;
+
+# I wanted to have this set the environment for configparser if debug was enabled *in configparser*
 if 'TERM_PROGRAM' in os.environ.keys() and os.environ['TERM_PROGRAM'] == 'vscode':
     os.chdir(os.path.expanduser('~\\Desktop\\Code'))
-
+    debug = True
+else: debug = False
 
 # check if window is fullscreen
 # this was originally stolen from stackoverflow but it didn't work correctly so I stole it from chatgpt instead
@@ -25,7 +40,7 @@ def is_fullscreen():
 
 config = configparser.ConfigParser(allow_no_value=True)
 
-# checks if config file exists, if it doesn't, create new one with booleanss, if it does, read it
+# checks if config file exists, if it doesn't, create new one with the below defaults, if it does, read it
 if not os.path.isfile('cfg.ini'):
     # defauits
     config['booleans'] = {'# change to True for on, False for off':None, 
@@ -37,7 +52,7 @@ if not os.path.isfile('cfg.ini'):
                           'archiveteam VM on/off':True, 
                           'archiveteam VM turns off after idle ends':True, 
                           'ytsync on/off':True}
-    config['paths'] = {'# you do not need to fill in ones that are turned off in the boolean section!':None, 
+    config['paths'] = {'# you do not need to fill in ones that are turned off in the boolean section':None, 
                        'wireguard executable path':r'C:\Program Files\WireGuard\wireguard.exe', 
                        'wireguard config path':r'C:\Program Files\WireGuard\Data\Configurations\wgcf-profile.conf.dpapi', 
                        'qbittorrent executable path':r'C:\Program Files\qBittorrent\qbittorrent.exe', 
@@ -50,28 +65,43 @@ if not os.path.isfile('cfg.ini'):
                       '# example: "URL 1 = https://www.google.com/"':None,
                       'URL 1':'',
                       'URL 2':'',
-                      'URL 3':''}
+                      'URL 3':'',
+                      '# do not go idle if the following executables are running':None,
+                      '# example: "blacklist 1 = AutoHotkey.exe"':None,
+                      'blacklist 1':'',
+                      'blacklist 2':'',
+                      'blacklist 3':''}
     # creating file requires admin, reading might too
     config.write(open('cfg.ini', 'w'))
 else: config.read('cfg.ini')
 
+# running executables blacklist
+def blacklisted():
+    if checkIfProcessRunning(config['misc']['blacklist 1']) or checkIfProcessRunning(config['misc']['blacklist 2']) or checkIfProcessRunning(config['misc']['blacklist 3']):
+        return True
+    else: return False
+
+# more optimizable code
+# TODO, use an actual timer library rather than trying to make a crap timer that lags every time you lag
 while(True): # this is so that the idle checking still continues if the computer becomes (temporarily) active
-    fixedposition = win32api.GetCursorPos() # a lot of this type of code could probably be put into a dedicated function but I can't be bothered
+    fixedposition = win32api.GetCursorPos() # a lot of this type of code could probably be put into a dedicated function but I can't be bothered, TODO
     mousetimer = 0
     # increases mousetimer until it reaches a set value, mouse movement resets it to 0
     while(mousetimer < int(config['misc']['time before idle'])):
         position = win32api.GetCursorPos()
+        # the below two if statements could probably be merged, TODO
         if fixedposition != position:
             mousetimer = 0
             fixedposition = win32api.GetCursorPos()
-        if config['booleans']['do not detect idle while fullscreen'] == 'True' and is_fullscreen():
+        if (config['booleans']['do not detect idle while fullscreen'] == 'True' and is_fullscreen()) or blacklisted():
             mousetimer = 0
             # mousetimer will constantly be at 0.05 if fullscreen because of above and below but it does not matter
-        mousetimer = round((mousetimer + 0.05), 2) # rounding is to remove floating point errors, not very important but it looks nice
+        if debug: print(mousetimer)
+        mousetimer = round((mousetimer + 0.05), 2) # rounding is to remove floating point errors, does not affect anything but it looks nice
         sleep(0.05)
 
     # runs the programs, REQUIRES ADMIN
-    # these should be done without the repeated if statements but I (mistakenly) thought it wouldn't matter
+    # these should be done without the repeated if statements but I (mistakenly) thought it wouldn't matter, TODO
     # subprocess doesn't like working in the background so popen is used instead
     # apparently True != 'True' and I wish I knew that yesterday before I changed all the code
     if config['booleans']['wireguard on/off'] == 'True':
@@ -83,6 +113,8 @@ while(True): # this is so that the idle checking still continues if the computer
     if config['booleans']['ytsync on/off'] == 'True':
         os.popen(fr'"{config['paths']['ytsync path']}"')
     # I tried to make this just loop 3 times but I got overwhelmed so you get more redundant code
+
+    # savepagenow does not work with task scheduler, turn it off if you use task scheduler!
     if config['misc']['URL 1']:
         savepagenow.capture_or_cache(config['misc']['URL 1'])
     if config['misc']['URL 2']:
@@ -91,7 +123,7 @@ while(True): # this is so that the idle checking still continues if the computer
         savepagenow.capture_or_cache(f'{config['misc']['URL 3']}')
 
     # for closing programs when coming out of idle, adapted from stackoverflow
-    # also could be done without the repeated if statements
+    # also could be done without the repeated if statements, TODO
     fixedposition = win32api.GetCursorPos()
     while(True):
         position = win32api.GetCursorPos()
