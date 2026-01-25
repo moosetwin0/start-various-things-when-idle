925de84e8f0fe8ff334eaf42b9175d22cad22d70
moosetwin0 <82544732+moosetwin0@users.noreply.github.com>
2026-01-24 21:05:21 -0500

    bugfixes, plus someone else's code
    - fixed fullscreen detection (that's the "someone else's code")
    - fixed program blocking standby mode

--- start various things when idle.py
+++ start various things when idle.py
@@ -1,4 +1,4 @@
-# start-various-things-when-idle Linux v1
+# start-various-things-when-idle Linux v2
 
 
 
@@ -8,7 +8,8 @@ from time import sleep
 from sys import argv # to check for debug flag # !!! can be removed if unused
 import psutil # checking if a process is running
 from subprocess import Popen # to open/close programs
-from Xlib.display import Display # for checking if fullscreen
+from FullscreenDetector import window_is_fullscreen
+from Xlib.display import Display # for getting pointer position, already needed by FullscreenDetector anyways
 
 # I initially copied it from https://thispointer.com/python-check-if-a-process-is-running-by-name-and-find-its-process-id-pid/ but now that I know what I know I could do it myself
 def checkIfProcessRunning(processName):
@@ -19,19 +20,6 @@ def checkIfProcessRunning(processName):
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
     return False;
-
-# taken from https://stackoverflow.com/a/67057803
-screen = Display().screen()
-root_window = screen.root
-def is_fullscreen():
-    for window in root_window.query_tree()._data['children']: # for each window open
-        width = window.get_geometry()._data["width"] # get its width
-        height = window.get_geometry()._data["height"] # get its height
-        if width == screen.width_in_pixels and height == screen.height_in_pixels: 
-            if not 'guard window' in window.get_wm_name(): # would always be fullscreen otherwise
-                if debug: print(window.get_wm_name())
-                return True # if it equals the monitor's width/height, return True
-    return False # else, return False
 
 # running executables blacklist
 def blacklisted():
@@ -51,30 +39,27 @@ except IndexError: pass
 if ('TERM_PROGRAM' in os.environ.keys() and os.environ['TERM_PROGRAM'] == 'vscode'):
     debug = True
 
-# works on linux !!! remove comment for release
 def getmousepos(): # shorthand
-    mouseinfo = screen.root.query_pointer() # probably could merge this line with the return but idk how
+    mouseinfo = Display().screen().root.query_pointer() # probably could merge this line with the return but idk how
     return mouseinfo.root_x, mouseinfo.root_y
 
-# works on linux !!! remove comment for release
 config = configparser.ConfigParser(allow_no_value=True)
 
-# !!! may need updating based on code below
 # checks if config file exists, if it doesn't, create new one with the below defaults, if it does, read it
 while True: # for updating config file
     if not os.path.isfile('cfg.ini'):
         # defaults
         # basedpyright gives a reportArgumentType error here but the code works fine
         # !!! couldn't get wireguard to work so it is commented out, and qbittorrent is too for safety reasons
-        config['toggles'] = {'don\'t go idle while in fullscreen':True,
+        config['toggles'] = {'don\'t go idle while in fullscreen':True,  # pyright: ignore[reportArgumentType]
                             #'wireguard':'on', 
                             #'qbittorrent':'on',
                             'VM':'on',
                             'ytdlp':'on'}
-        config['paths'] = {'# the paths for any unused features can be left blank':None, 
+        config['paths'] = {'# the paths for any unused features can be left blank':None,  # pyright: ignore[reportArgumentType]
                         #'wireguard config name':'wgcf-profile', 
                         'ytdlp path':'~/Music/yt-dlp/yt-dlp'}
-        config['misc'] = {'vm name':'archiveteam-warrior-4.2', 
+        config['misc'] = {'vm name':'archiveteam-warrior-4.2',  # pyright: ignore[reportArgumentType]
                         'seconds before idle':'600', 
                         '# how long it should wait before checking if the mouse has moved':None,
                         '# low values will cause performance issues!':None,
@@ -109,8 +94,8 @@ while(True): # this is so that the idle checking still continues if the computer
             mousetimer = 0
             startpos = getmousepos()
         # having it only check the var once (instead of every loop) might be better on performance but it'd be more complicated
-        if (config['toggles'].getboolean('don\'t go idle while in fullscreen') and is_fullscreen()) or blacklisted():
-            if debug: print(f'fullscreen: {is_fullscreen()}, blacklisted: {blacklisted()}')
+        if (config['toggles'].getboolean('don\'t go idle while in fullscreen') and window_is_fullscreen()) or blacklisted():
+            if debug: print(f'-- fullscreen: {window_is_fullscreen()}, blacklisted: {blacklisted()} --')
             mousetimer = 0
             # mousetimer will constantly be at 1 if fullscreen because of above and below but it does not matter
         if debug: print(mousetimer)
@@ -134,13 +119,16 @@ while(True): # this is so that the idle checking still continues if the computer
         # subprocess doesn't work with ~/ so expanduser is used
         ytdlppath = os.path.expanduser(config['paths']['ytdlp path'])
         _ = Popen(ytdlppath, cwd=os.path.dirname(ytdlppath))
+    sleep(5)
+    _ = Popen('xset dpms force standby', shell=True) # manually turn off the monitor, otherwise programs would keep it on
 
     # for closing programs when coming out of idle, adapted from stackoverflow
     # also could be done without the repeated if statements, TODO
     while(True):
         if startpos != getmousepos():
+            if debug: print('-- shutting down --')
             if config['toggles'].getboolean('VM'):
-                _ = Popen(f'vboxmanage controlvm "{config['misc']['vm name']}" acpipowerbutton'.split())
+                _ = Popen(f'vboxmanage controlvm {config['misc']['vm name']} acpipowerbutton'.split())
             # !!! fix wireguard sudo, see changelog
             #if config['toggles'].getboolean('qbittorrent'):
                 #qbit.terminate() # pyright: ignore[reportPossiblyUnboundVariable]
